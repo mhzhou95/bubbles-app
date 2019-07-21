6703a7f1b7f2a8b924d3c0e18eefe991ceff8fe5
unknown <minghaozhou123@gmai.com>
2019-07-20 23:25:13 -0400

    basic navbar with template signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar.component';
+import Home from './components/Home.component';
+import Login from './components/Login.component';
+import Signup from './components/Signup.component';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
