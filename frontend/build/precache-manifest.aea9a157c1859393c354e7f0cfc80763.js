bfe1d3a0fccba4df90ff7092eaafc71710fc1ba6
unknown <minghaozhou123@gmai.com>
2019-07-23 16:37:35 -0400

    build

---
+++ frontend/build/precache-manifest.aea9a157c1859393c354e7f0cfc80763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945e7e4de14b9a12c25c49f24e369c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1054a92d267d47fbdf36",
+    "url": "/static/css/main.abf56d19.chunk.css"
+  },
+  {
+    "revision": "e640e5a81ed43d47fba7",
+    "url": "/static/js/2.61286d34.chunk.js"
+  },
+  {
+    "revision": "1054a92d267d47fbdf36",
+    "url": "/static/js/main.1f5fd825.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
