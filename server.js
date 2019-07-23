f7371788e9773fd3cfba9d9a62b7b60b89d6ac5b
unknown <minghaozhou123@gmai.com>
2019-07-23 16:31:27 -0400

    ..

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // call required modules
 const express = require('express');
+
 const cors = require('cors');
+const path = require('path');
 // mongoose to connect to mongo DB
 const mongoose = require('mongoose');
 
@@ -9,7 +11,7 @@ require('dotenv').config();
 
 //create express server and port
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 // cors middleware
 app.use(cors());
@@ -40,6 +42,12 @@ app.use('/api/bubbles', exercisesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+const port = process.env.PORT || 5000;
 // starts server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
